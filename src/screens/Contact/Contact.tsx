763d50ba3ecedce742300feb53bbf9a53ca4c050
Jean-Michel Keuleyan <jm.keuleyan@gmail.com>
2024-01-27 17:53:46 +0100

    Add scrollview to contacts

--- src/screens/Contact/Contact.tsx
+++ src/screens/Contact/Contact.tsx
@@ -2,7 +2,7 @@ import { useNavigation } from "@react-navigation/native";
 import * as Clipboard from "expo-clipboard";
 import * as WebBrowser from "expo-web-browser";
 import React, { useEffect } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import Accordion from "react-native-collapsible/Accordion";
 import { WithLocalSvg } from "react-native-svg";
 import CustomView from "../../components/CustomView";
@@ -45,193 +45,195 @@ const Contact = () => {
           Contact
         </Text>
       </Headerbar>
-      <View
-        style={{
-          flex: 1,
-          paddingHorizontal: adjustSize(20),
-          marginTop: adjustSize(20),
-        }}
-      >
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            gap: adjustSize(10),
-          }}
-        >
-          <TouchableOpacity
-            activeOpacity={3}
-            onPress={async () => {
-              await Clipboard.setString("Johndoe123@ gmail.com");
-              Toast.show({
-                type: "success",
-                text2: "Email Copied",
-                position: "bottom",
-              });
-            }}
-            style={{
-              flex: 1,
-              height: adjustSize(130),
-              backgroundColor: colors.secondary,
-              borderRadius: adjustSize(13),
-              alignItems: "center",
-              justifyContent: "space-evenly",
-            }}
-          >
-            <WithLocalSvg
-              asset={require("../../../assets/Svg/Message.svg")}
-              height={adjustSize(25)}
-              width={adjustSize(25)}
-            />
-            <View
-              style={{
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: adjustSize(15),
-                  fontWeight: "500",
-                  color: colors.secondaryLight,
-                  marginBottom: adjustSize(5),
-                }}
-              >
-                Email us at
-              </Text>
-              <Text
-                style={{
-                  fontSize: adjustSize(11),
-                  fontWeight: "400",
-                  color: colors.white,
-                }}
-              >
-                Johndoe123@ gmail.com
-              </Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            activeOpacity={4}
-            onPress={() => {
-              WebBrowser.openBrowserAsync("https://wa.me/+33663219604");
-            }}
-            style={{
-              flex: 1,
-
-              height: adjustSize(130),
-              backgroundColor: colors.secondary,
-              borderRadius: adjustSize(13),
-              alignItems: "center",
-              justifyContent: "space-evenly",
-            }}
-          >
-            <WithLocalSvg
-              asset={require("../../../assets/Svg/whatsapp.svg")}
-              height={adjustSize(25)}
-              width={adjustSize(25)}
-            />
-            <View
-              style={{
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: adjustSize(14),
-                  fontWeight: "500",
-                  color: colors.secondaryLight,
-                  marginBottom: adjustSize(5),
-                }}
-              >
-                WhatsApp Us
-              </Text>
-              <Text
-                style={{
-                  fontSize: adjustSize(11),
-                  fontWeight: "400",
-                  color: colors.white,
-                }}
-              >
-                +1 234567890
-              </Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-
+      <ScrollView>
         <View
           style={{
             flex: 1,
+            paddingHorizontal: adjustSize(20),
             marginTop: adjustSize(20),
           }}
         >
-          <Accordion
-            sections={SECTIONS}
-            activeSections={activeSections}
-            renderHeader={({ title }, index, isActive) => {
-              return (
-                <View
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: adjustSize(10),
+            }}
+          >
+            <TouchableOpacity
+              activeOpacity={3}
+              onPress={async () => {
+                await Clipboard.setString("support@fast-transfer.app");
+                Toast.show({
+                  type: "success",
+                  text2: "Email Copied",
+                  position: "bottom",
+                });
+              }}
+              style={{
+                flex: 1,
+                height: adjustSize(130),
+                backgroundColor: colors.secondary,
+                borderRadius: adjustSize(13),
+                alignItems: "center",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <WithLocalSvg
+                asset={require("../../../assets/Svg/Message.svg")}
+                height={adjustSize(25)}
+                width={adjustSize(25)}
+              />
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Text
                   style={{
-                    flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                    paddingVertical: adjustSize(15),
-                    borderBottomWidth:
-                      index === SECTIONS.length - 1 ? 0 : isActive ? 0 : 0.5,
-                    borderBottomColor: colors.secondaryLight,
+                    fontSize: adjustSize(15),
+                    fontWeight: "500",
+                    color: colors.secondaryLight,
+                    marginBottom: adjustSize(5),
                   }}
                 >
-                  <Text
-                    style={{
-                      fontSize: adjustSize(14),
-                      fontWeight: "600",
-                      color: colors.white,
-                    }}
-                  >
-                    {title}
-                  </Text>
-                  {!isActive ? (
-                    <WithLocalSvg
-                      asset={require("../../../assets/Svg/right_arrow.svg")}
-                    />
-                  ) : (
-                    <WithLocalSvg
-                      asset={require("../../../assets/Svg/chevron-down.svg")}
-                      color={colors.white}
-                    />
-                  )}
-                </View>
-              );
-            }}
-            renderContent={({ answer }, index, isActive) => {
-              return (
-                <View
+                  Email us at
+                </Text>
+                <Text
                   style={{
-                    paddingVertical: adjustSize(2),
-                    borderBottomWidth:
-                      index === SECTIONS.length - 1 ? 0 : !isActive ? 0 : 0.5,
-                    borderBottomColor: colors.secondaryLight,
-                    paddingBottom: adjustSize(10),
+                    fontSize: adjustSize(11),
+                    fontWeight: "400",
+                    color: colors.white,
                   }}
                 >
-                  <Text
+                  support@fast-transfer.app
+                </Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+              activeOpacity={4}
+              onPress={() => {
+                WebBrowser.openBrowserAsync("https://wa.me/+33663219604");
+              }}
+              style={{
+                flex: 1,
+
+                height: adjustSize(130),
+                backgroundColor: colors.secondary,
+                borderRadius: adjustSize(13),
+                alignItems: "center",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <WithLocalSvg
+                asset={require("../../../assets/Svg/whatsapp.svg")}
+                height={adjustSize(25)}
+                width={adjustSize(25)}
+              />
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: adjustSize(14),
+                    fontWeight: "500",
+                    color: colors.secondaryLight,
+                    marginBottom: adjustSize(5),
+                  }}
+                >
+                  WhatsApp Us
+                </Text>
+                <Text
+                  style={{
+                    fontSize: adjustSize(11),
+                    fontWeight: "400",
+                    color: colors.white,
+                  }}
+                >
+                  +33 6 63 21 96 04
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <View
+            style={{
+              flex: 1,
+              marginTop: adjustSize(20),
+            }}
+          >
+            <Accordion
+              sections={SECTIONS}
+              activeSections={activeSections}
+              renderHeader={({ title }, index, isActive) => {
+                return (
+                  <View
                     style={{
-                      fontSize: adjustSize(13),
-                      fontWeight: "500",
-                      color: colors.secondaryLight,
+                      flexDirection: "row",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      paddingVertical: adjustSize(15),
+                      borderBottomWidth:
+                        index === SECTIONS.length - 1 ? 0 : isActive ? 0 : 0.5,
+                      borderBottomColor: colors.secondaryLight,
                     }}
                   >
-                    {answer}
-                  </Text>
-                </View>
-              );
-            }}
-            onChange={(activeSections) => {
-              setActiveSections(activeSections);
-            }}
-          />
+                    <Text
+                      style={{
+                        fontSize: adjustSize(14),
+                        fontWeight: "600",
+                        color: colors.white,
+                      }}
+                    >
+                      {title}
+                    </Text>
+                    {!isActive ? (
+                      <WithLocalSvg
+                        asset={require("../../../assets/Svg/right_arrow.svg")}
+                      />
+                    ) : (
+                      <WithLocalSvg
+                        asset={require("../../../assets/Svg/chevron-down.svg")}
+                        color={colors.white}
+                      />
+                    )}
+                  </View>
+                );
+              }}
+              renderContent={({ answer }, index, isActive) => {
+                return (
+                  <View
+                    style={{
+                      paddingVertical: adjustSize(2),
+                      borderBottomWidth:
+                        index === SECTIONS.length - 1 ? 0 : !isActive ? 0 : 0.5,
+                      borderBottomColor: colors.secondaryLight,
+                      paddingBottom: adjustSize(10),
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontSize: adjustSize(13),
+                        fontWeight: "500",
+                        color: colors.secondaryLight,
+                      }}
+                    >
+                      {answer}
+                    </Text>
+                  </View>
+                );
+              }}
+              onChange={(activeSections) => {
+                setActiveSections(activeSections);
+              }}
+            />
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </CustomView>
   );
 };
